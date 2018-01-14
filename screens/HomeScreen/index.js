085762d1783da4d065b2a7eb98a8403f454b03d4
juan <juananime@gmail.com>
2018-01-14 12:08:33 +0100

    setup flow/prettier/eslint

--- screens/HomeScreen/index.js
+++ screens/HomeScreen/index.js
@@ -1,16 +1,12 @@
 // @flow
 import React, { PureComponent } from "react";
-type PropsType = $Exact<{
-
-}>;
+type PropsType = $Exact<{}>;
 type StateType = $Exact<{
-       bla:string;
-       
+  bla: string
 }>;
 
 class HomeScreen extends PureComponent<PropsType, StateType> {
-    constructor(props: PropsType) {
-        super(props);
-
-    }
+  constructor(props: PropsType) {
+    super(props);
+  }
 }
